a23e60b109b826076c059fa64919e744cd322648
WES <21105311+wesimon01@users.noreply.github.com>
2024-09-01 01:24:03 -0400

    Added PrimeVue to project

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import Knob from 'primevue/knob'
+import { ref } from 'vue';
+
+const knobVal = ref(0);
 </script>
 
 <template>
   <header>
+    <Knob v-model="knobVal" :step="1"></Knob>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+    
     <div class="wrapper">
       <HelloWorld msg="You did it! Hello dawg" />
-
+      
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
